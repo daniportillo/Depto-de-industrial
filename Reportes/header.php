cf7cccd8f123ddc5fecf0dd1ccb02cacda92e29b
daniportillo <armdan22@gmail.com>
2015-08-03 19:02:44 -0700

    Cambios menu

--- Reportes/header.php
+++ Reportes/header.php
@@ -37,36 +37,44 @@ if(!isset($_SESSION['nombre']))
 </div>
 
 <header>
-
-</nav>
-<div class="menu_bar">
-      <a href="#" class="bt-menu"><span class="icon-list2"></span>Menu</a>
-</div>
  
-    <nav>
-      <ul>
-        <!--Menu para todo usuarios-->
-        <li><a href="index.php"><img src="iconos/inicio.png" width="17" height="17" border="0"/> Inicio</a></li>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+          <div class="navbar-header">
+             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+             </button>
+           </div>
+           <div class="collapse navbar-collapse" id="menu">
+             <ul class="nav navbar-nav">
+                <!--Menu para todo usuarios-->
+               <li><a href="index.php"><img src="iconos/inicio.png" width="17" height="17" border="0"/> Inicio</a></li>
        
-        <!--Menu solo para administradores-->
-        <?php if ($_SESSION['tipo']=='administrador') {
-        echo " 
-        <li><a href='nuevo_usuario.php'><img src='iconos/nuevousuario.png' width='18' height='18'   border='0'/>  Nuevo Usuario</a></li>
-        <li><a href='configuracion.php'><img src='iconos/configuracion.png' width='18' height='18' border='0'/>  Configuracion</a></li>
-        <li><a href='reportes.php'><img src='iconos/nuevoreporte.png' width='18' height='18' alt='Prueba'  border='0'/>  Reportes</a></li>";
-        } ?>
+                 <!--Menu solo para administradores-->
+                   <?php if ($_SESSION['tipo']=='administrador') {
+                     echo " 
+                       <li><a href='nuevo_usuario.php'><img src='iconos/nuevousuario.png' width='18' height='18'   border='0'/>  Nuevo Usuario</a></li>
+                       <li><a href='configuracion.php'><img src='iconos/configuracion.png' width='18' height='18' border='0'/>  Configuracion</a></li>
+                       <li><a href='reportes.php'><img src='iconos/nuevoreporte.png' width='18' height='18' alt='Prueba'  border='0'/>  Reportes</a></li>";
+                   } ?>
         
-        <!--Menu para usuarios normales-->
-        <?php if ($_SESSION['tipo']=="usuario") {
-        echo "
-        <li><a href='nuevoreporte.php''><img src='iconos/nuevoreporte.png' width='18' height='18'  border='0'/>Nuevo Reporte</a></li>
-        <li><a href='configuracion_personal.php''><img src='iconos/configuracion.png' width='18' height='18' alt='Prueba' title='Prueba' border='0'/>Configuración personal</a></li>";
+                 <!--Menu para usuarios normales-->
+                   <?php if ($_SESSION['tipo']=="usuario") {
+                   echo "
+                   <li><a href='nuevoreporte.php''><img src='iconos/nuevoreporte.png' width='18' height='18'  border='0'/>Nuevo Reporte</a></li>
+                   <li><a href='configuracion_personal.php''><img src='iconos/configuracion.png' width='18' height='18' alt='Prueba' title='Prueba' border='0'/>Configuración personal</a></li>";
+                    } ?>
         
-        } ?>
-        
-        <!--Menu para todo usuarios-->
-        <li><a href=""><img src="iconos/acerca.png" width="18" height="18" alt="Prueba"  border="0"> Acerca de</a></li>
-        <li><a href="logout.php"><img src="iconos/cerrarsesion.png" width="18" height="18" border="0"/>   Cerrar Sesion</a></li>
-      </ul>
+                 <!--Menu para todo usuarios-->
+               <li><a href=""><img src="iconos/acerca.png" width="18" height="18" border="0"> Acerca de</a></li>
+             </ul>
+             <ul class="nav navbar-nav navbar-right">
+              <li> <a href=""> <span class="glyphicon glyphicon-user"></span><?php echo '  '.$name; ?></a></li>
+               <li><a href="logout.php"><img src="iconos/cerrarsesion.png" width="18" height="18" border="0"/>   Cerrar Sesion</a></li>
+            </ul>
+          </div>
+      </div>
     </nav>
 </header>
